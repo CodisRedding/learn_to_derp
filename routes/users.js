cb035c4cbf8c910008d2a37c887a343a7fb0f4ab
Rocky Assad <r@cky.email>
2015-11-09 10:33:43 -0600

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+
+var iamprivate = 1;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  if (req.body.shouldBePrivate === 1 || iamprivate === 1) {
+    crypto.doSpecialThings();
+    iamprivate = 2;
+  }
   res.send('respond with a resource');
 });
 
